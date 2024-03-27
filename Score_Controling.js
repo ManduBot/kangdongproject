9d0034d821e7f4393e02a4f3f06aef765b64eb5d
ManduBot <99074446+ManduBot@users.noreply.github.com>
2024-03-27 14:52:55 +0900

    Update Score_Controling.js

--- Score_Controling.js
+++ Score_Controling.js
@@ -1,11 +1,11 @@
-function score(){
-var left = [document.querySelectorAll('[id="m'+document.getElementById('left_card_0').getElementsByClassName("cc vol_on")[0].getAttribute('data-idx')+'"][class="match-meaning"]')[0].innerText,document.querySelectorAll('[id="m'+document.getElementById('left_card_1').getElementsByClassName("cc vol_on")[0].getAttribute('data-idx')+'"][class="match-meaning"]')[0].innerText,document.querySelectorAll('[id="m'+document.getElementById('left_card_2').getElementsByClassName("cc vol_on")[0].getAttribute('data-idx')+'"][class="match-meaning"]')[0].innerText,document.querySelectorAll('[id="m'+document.getElementById('left_card_3').getElementsByClassName("cc vol_on")[0].getAttribute('data-idx')+'"][class="match-meaning"]')[0].innerText]
-var right = [document.getElementById('right_card_0').innerText,document.getElementById('right_card_1').innerText,document.getElementById('right_card_2').innerText,document.getElementById('right_card_3').innerText]
-for (var i in left){
-for (var n in right){
-if (left[i].indexOf(right[n])!=-1){
-console.log((Number(i)+1)+','+(Number(n)+1))
-}
-}
-}
+function showAnswer(){
+  var left = [document.querySelectorAll('[id="m'+document.getElementById('left_card_0').getElementsByClassName("cc vol_on")[0].getAttribute('data-idx')+'"][class="match-meaning"]')[0].innerText,document.querySelectorAll('[id="m'+document.getElementById('left_card_1').getElementsByClassName("cc vol_on")[0].getAttribute('data-idx')+'"][class="match-meaning"]')[0].innerText,document.querySelectorAll('[id="m'+document.getElementById('left_card_2').getElementsByClassName("cc vol_on")[0].getAttribute('data-idx')+'"][class="match-meaning"]')[0].innerText,document.querySelectorAll('[id="m'+document.getElementById('left_card_3').getElementsByClassName("cc vol_on")[0].getAttribute('data-idx')+'"][class="match-meaning"]')[0].innerText]
+  var right = [document.getElementById('right_card_0').innerText,document.getElementById('right_card_1').innerText,document.getElementById('right_card_2').innerText,document.getElementById('right_card_3').innerText]
+  for (var i in left){
+    for (var n in right){
+      if (left[i].indexOf(right[n])!=-1){
+        console.log((Number(i)+1)+','+(Number(n)+1))
+      }
+    }
+  }
 }
